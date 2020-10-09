adde0fa6fb92266d1536ac8a08ef0b8e037d87d1
Brian Barton <brian.n.barton@gmail.com>
2020-10-09 08:41:15 -0600

    jpa and repositories

---
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -0,0 +1,85 @@
+package com.codeup.blog.controllers;
+
+import com.codeup.blog.models.Post;
+import com.codeup.blog.repository.PostRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class PostController {
+    private final PostRepository postRepo;
+
+    public PostController(PostRepository postRepo) {
+        this.postRepo = postRepo;
+    }
+
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    public String showAllPosts(Model model) {
+        model.addAttribute("posts", postRepo.findAll());
+        return "posts/index";
+    }
+
+    @GetMapping("/posts/{id}")
+    public String showOnePost(@PathVariable long id, Model model) {
+        Post post = postRepo.getAdById(id);
+        model.addAttribute("post", post);
+        return "posts/show";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    public String createPostForm() {
+        return "posts/create";
+    }
+
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    public String createPost(@RequestParam(name = "title") String title,
+                             @RequestParam(name = "body") String body,
+                             Model model) {
+        Post post = new Post();
+        post.setTitle(title);
+        post.setBody(body);
+        postRepo.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
+
+
+    @GetMapping("/posts/delete/{id}")
+    public String deletePost(@PathVariable long id, Model model) {
+        Post post = postRepo.getAdById(id);
+        if (post != null) {
+            postRepo.delete(post);
+        }
+        return "redirect:/posts";
+    }
+
+
+    @GetMapping("/posts/edit/{id}")
+    public String showEditPost(@PathVariable long id, Model model) {
+        Post post = postRepo.getAdById(id);
+        if (post == null) {
+            return "redirect:/posts/index";
+        }
+        model.addAttribute("post", post);
+        return "posts/edit";
+    }
+
+
+    @PostMapping("/posts/edit")
+    public String updatePost(@RequestParam(name = "id") long id,
+                             @RequestParam(name = "title") String title,
+                             @RequestParam(name = "body") String body,
+                             Model model) {
+        Post post = postRepo.getAdById(id);
+        if (post == null) {
+            return "redirect:/posts/index";
+        }
+        post.setTitle(title);
+        post.setBody(body);
+        postRepo.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
+}
--- src/main/java/com/codeup/demo/repositories/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codeup.demo.repositories;
-
-import com.codeup.demo.models.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository <Post, Long>{
-}
