4e070e35509367ffc4b2d6b272ddaaf8d13b4fff
Brian Barton <brian.n.barton@gmail.com>
2020-10-13 07:48:59 -0600

    relationships

--- src/main/java/com/codeup/blog/BlogApplication.java
+++ src/main/java/com/codeup/blog/BlogApplication.java
@@ -1,13 +1,90 @@
-package com.codeup.blog;
+package com.codeup.blog.controllers;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.codeup.blog.models.Ad;
 
-@SpringBootApplication
-public class BlogApplication {
+import com.codeup.blog.models.AdCategory;
+import com.codeup.blog.repositories.AdRepository;
+import com.codeup.blog.repositories.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-    public static void main(String[] args) {
-        SpringApplication.run(BlogApplication.class, args);
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class AdController {
+    private final AdRepository adDao;
+    private final UserRepository userDao;
+
+    public AdController(AdRepository adDao, UserRepository userDao) {
+        this.adDao = adDao;
+        this.userDao = userDao;
     }
 
+    @RequestMapping(path = "/ads", method = RequestMethod.GET)
+    public String showAllAds(Model model) {
+        model.addAttribute("ads", adDao.findAll());
+        return "ads/index";
+    }
+
+    @RequestMapping(path = "/ads/{id}", method = RequestMethod.GET)
+    public String showSingleAd(@PathVariable long id, Model model) {
+        model.addAttribute("ad", adDao.getOne(id));
+        return "ads/show";
+    }
+
+    @GetMapping("ads/hardcoded/create")
+    public String createHardcodedAd() {
+        Ad ad = new Ad();
+        ad.setTitle("Title to Chevy Silverado, Title only.");
+        ad.setDescription("Selling the title to Daddy's Silverado. His car was lost at sea.");
+        ad.setCategories(new ArrayList<AdCategory>());
+        ad.setOwner(userDao.getOne(1L));
+        adDao.save(ad);
+        return "redirect:/ads";
+    }
+
+    @GetMapping("/ads/create")
+    public String showCreateView() {
+        return "ads/create";
+    }
+
+    @PostMapping("/ads/create")
+    public String createAd(@RequestParam(name = "title") String title,
+                           @RequestParam(name = "description") String description,
+                           Model model) {
+        Ad ad = new Ad(title, description);
+        adRepo.save(ad);
+        return "redirect:/ads/" + ad.getId();
+    }
+
+    @GetMapping("/ads/delete/{id}")
+    public String deleteAd(@PathVariable long id, Model model) {
+        Ad ad = adRepo.getAdById(id);
+        adRepo.delete(ad);
+        return "redirect:/ads";
+    }
+
+    @GetMapping("/ads/edit/{id}")
+    public String editAd(@PathVariable long id, Model model) {
+        Ad ad = adRepo.getAdById(id);
+        model.addAttribute("ad", ad);
+        return "ads/edit";
+    }
+
+    @PostMapping("/ads/edit")
+    public String updateAd(@RequestParam(name = "id") long id,
+                           @RequestParam(name = "title") String title,
+                           @RequestParam(name = "description") String description) {
+        Ad ad = new Ad();
+        ad.setId(id);
+        ad.setTitle(title);
+        ad.setDescription(description);
+        adRepo.save(ad);
+        return "redirect:/ads/" + ad.getId();
+    }
 }
